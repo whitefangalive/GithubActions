7eeb9eb96f3a107d998f343fbbec73bd46dbfa12
Kyle Gann <kgann2@huskers.unl.edu>
2024-04-12 14:37:13 -0500

    added last name

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_ValidGann()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_InvalidGann()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_NullGann()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,72 +32,72 @@ namespace GithubActionsLab
 
         // Implement 3 tests per operation, following a similar pattern as above
         [Test]
-        public void Subtract_Valid()
+        public void Subtract_ValidGann()
         {
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
         }
 
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_InvalidGann()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
         }
 
         [Test]
-        public void Subtract_Null()
+        public void Subtract_NullGann()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
         }
 
         [Test]
-        public void Multiply_Valid()
+        public void Multiply_ValidGann()
         {
             Assert.AreEqual(2, Program.Multiply("1", "2"));
         }
 
         [Test]
-        public void Multiply_Invalid()
+        public void Multiply_InvalidGann()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
         }
 
         [Test]
-        public void Multiply_Null()
+        public void Multiply_NullGann()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
         }
 
         [Test]
-        public void Divide_Valid()
+        public void Divide_ValidGann()
         {
             Assert.AreEqual(1, Program.Divide("2", "2"));
         }
 
         [Test]
-        public void Divide_Invalid()
+        public void Divide_InvalidGann()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
         }
 
         [Test]
-        public void Divide_Null()
+        public void Divide_NullGann()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
         }
         [Test]
-        public void Power_Valid()
+        public void Power_ValidGann()
         {
             Assert.AreEqual(4, Program.Power("2", "2"));
         }
 
         [Test]
-        public void Power_Invalid()
+        public void Power_InvalidGann()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
         }
 
         [Test]
-        public void Power_Null()
+        public void Power_NullGann()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
         }
